a3e528944cd8aaa326769ba8314ac32355b49c05
Simon Herrera <simonherrera.tech@gmail.com>
2017-02-01 17:22:05 -0600

    Added Nav and Footer to JS file - Changed background and Font

--- app/app.js
+++ app/app.js
@@ -1,22 +1,29 @@
-// var htmlLinks = document.getElementById('nav-links');
-// htmlLinks.innerHTML =
-//   '<nav class="navbar navbar-inverse navbar-fixed-top">' +
-//     '<div class="container">' +
-//       '<div class="navbar-header">' +
-//         '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">' +
-//           '<span class="sr-only">Toggle navigation</span>' +
-//           '<span class="icon-bar"></span>' +
-//           '<span class="icon-bar"></span>' +
-//           '<span class="icon-bar"></span>' +
-//         '</button>' +
-//         '<a class="navbar-brand" href="../index.html">Simon Herrera</a>' +
-//       '</div>' +
-//       '<div id="navbar" class="collapse navbar-collapse">' +
-//         '<ul class="nav navbar-nav">' +
-//           '<li><a href="app/projects.html">Projects</a></li>' +
-//           '<li><a href="app/about">About</a></li>' +
-//           '<li><a href="app/contact">Contact</a></li>' +
-//         '</ul>' +
-//       '</div>' +
-//     '</div>' +
-//   '</nav>';
+var htmlLinks = document.getElementById('nav-links');
+htmlLinks.innerHTML =
+  '<nav class="navbar navbar-inverse navbar-fixed-top">' +
+    '<div class="container">' +
+      '<div class="navbar-header">' +
+        '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">' +
+          '<span class="sr-only">Toggle navigation</span>' +
+          '<span class="icon-bar"></span>' +
+          '<span class="icon-bar"></span>' +
+          '<span class="icon-bar"></span>' +
+        '</button>' +
+        '<a class="navbar-brand" href="../index.html">Simon Herrera</a>' +
+      '</div>' +
+      '<div id="navbar" class="collapse navbar-collapse">' +
+        '<ul class="nav navbar-nav">' +
+          '<li><a href="../app/projects.html">Projects</a></li>' +
+          '<li><a href="../app/about">About</a></li>' +
+          '<li><a href="../app/contact">Contact</a></li>' +
+        '</ul>' +
+      '</div>' +
+    '</div>' +
+  '</nav>';
+
+  var footerBar = document.getElementById('footer-bar');
+  footerBar.innerHTML =
+    // '<p id="main-footer">&copy; 2016 Simon Herrera</p>';
+    '<article id="main-footer">' +
+      '<p>&copy; 2016 Simon Herrera</p>' +
+    '</article>';
